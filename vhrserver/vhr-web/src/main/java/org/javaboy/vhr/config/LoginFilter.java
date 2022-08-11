ff7ad9a3451f050e1741194a32a0b6f3bc6dbe9b
王一宁 <1506042595@qq.com>
2022-08-11 11:33:09 +0800

    实现用户登录

--- vhrserver/vhr-web/src/main/java/org/javaboy/vhr/config/LoginFilter.java
+++ vhrserver/vhr-web/src/main/java/org/javaboy/vhr/config/LoginFilter.java
@@ -25,17 +25,18 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         if (!request.getMethod().equals("POST")) {
             throw new AuthenticationServiceException(
-                    "AuthenticationException method not supported: " + request.getMethod());
+                    "Authentication method not supported: " + request.getMethod());
         }
-
+        String verify_code = (String) request.getSession().getAttribute("verify_code");
         if (request.getContentType().contains(MediaType.APPLICATION_JSON_VALUE) || request.getContentType().contains(MediaType.APPLICATION_JSON_UTF8_VALUE)) {
             Map<String, String> loginData = new HashMap<>();
             try {
                 loginData = new ObjectMapper().readValue(request.getInputStream(), Map.class);
             } catch (IOException e) {
-                e.printStackTrace();
+            }finally {
+                String code = loginData.get("code");
+                checkCode(response, code, verify_code);
             }
-
             String username = loginData.get(getUsernameParameter());
             String password = loginData.get(getPasswordParameter());
             if (username == null) {
@@ -52,8 +53,16 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
             principal.setUsername(username);
             sessionRegistry.registerNewSession(request.getSession(true).getId(), principal);
             return this.getAuthenticationManager().authenticate(authRequest);
-        }else{
+        } else {
+            checkCode(response, request.getParameter("code"), verify_code);
             return super.attemptAuthentication(request, response);
+        }
+    }
+
+    public void checkCode(HttpServletResponse resp, String code, String verify_code) {
+        if (code == null || verify_code == null || "".equals(code) || !verify_code.toLowerCase().equals(code.toLowerCase())) {
+            //验证码不正确
+            throw new AuthenticationServiceException("验证码不正确");
         }
     }
 }
