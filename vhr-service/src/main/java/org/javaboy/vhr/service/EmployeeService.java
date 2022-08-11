0c1a3f9753e4772f6be9ee50d08b969d2931eac8
王一宁 <1506042595@qq.com>
2022-08-11 21:11:33 +0800

    员工基本信息

--- vhr-service/src/main/java/org/javaboy/vhr/service/EmployeeService.java
+++ vhr-service/src/main/java/org/javaboy/vhr/service/EmployeeService.java
@@ -2,13 +2,9 @@ package org.javaboy.vhr.service;
 
 import org.javaboy.vhr.mapper.EmployeeMapper;
 import org.javaboy.vhr.model.Employee;
-import org.javaboy.vhr.model.MailConstants;
-import org.javaboy.vhr.model.MailSendLog;
 import org.javaboy.vhr.model.RespPageBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.connection.CorrelationData;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,25 +12,12 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
-/**
- * @作者 江南一点雨
- * @公众号 江南一点雨
- * @微信号 a_java_boy
- * @GitHub https://github.com/lenve
- * @博客 http://wangsong.blog.csdn.net
- * @网站 http://www.javaboy.org
- * @时间 2019-10-29 7:44
- */
 @Service
 public class EmployeeService {
     @Autowired
     EmployeeMapper employeeMapper;
-    @Autowired
-    RabbitTemplate rabbitTemplate;
-    @Autowired
-    MailSendLogService mailSendLogService;
+
     public final static Logger logger = LoggerFactory.getLogger(EmployeeService.class);
     SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
     SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
@@ -58,20 +41,6 @@ public class EmployeeService {
         double month = (Double.parseDouble(yearFormat.format(endContract)) - Double.parseDouble(yearFormat.format(beginContract))) * 12 + (Double.parseDouble(monthFormat.format(endContract)) - Double.parseDouble(monthFormat.format(beginContract)));
         employee.setContractTerm(Double.parseDouble(decimalFormat.format(month / 12)));
         int result = employeeMapper.insertSelective(employee);
-        if (result == 1) {
-            Employee emp = employeeMapper.getEmployeeById(employee.getId());
-            //生成消息的唯一id
-            String msgId = UUID.randomUUID().toString();
-            MailSendLog mailSendLog = new MailSendLog();
-            mailSendLog.setMsgId(msgId);
-            mailSendLog.setCreateTime(new Date());
-            mailSendLog.setExchange(MailConstants.MAIL_EXCHANGE_NAME);
-            mailSendLog.setRouteKey(MailConstants.MAIL_ROUTING_KEY_NAME);
-            mailSendLog.setEmpId(emp.getId());
-            mailSendLog.setTryTime(new Date(System.currentTimeMillis() + 1000 * 60 * MailConstants.MSG_TIMEOUT));
-            mailSendLogService.insert(mailSendLog);
-            rabbitTemplate.convertAndSend(MailConstants.MAIL_EXCHANGE_NAME, MailConstants.MAIL_ROUTING_KEY_NAME, emp, new CorrelationData(msgId));
-        }
         return result;
     }
 
